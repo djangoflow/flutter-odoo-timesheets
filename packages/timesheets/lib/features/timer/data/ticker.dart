19cf8bdf6d3ef9ddc04c5cffb6390d5edf506454
shata <shatanikmahanty@gmail.com>
2023-02-21 14:55:10 +0530

    (Refactor) Ticker renamed as TimeSheetTicker

--- packages/timesheets/lib/features/timer/data/ticker.dart
+++ packages/timesheets/lib/features/timer/data/ticker.dart
@@ -1,6 +1,5 @@
-// TODO name this to maybe TimeSheetTicker or something to avoid confusion with flutter's ticker
-class Ticker {
-  const Ticker();
+class TimeSheetTicker {
+  const TimeSheetTicker();
 
   Stream<int> tick(int goForward) => Stream.periodic(
       const Duration(seconds: 1), (x) => ((goForward - 1) + x + 1));
