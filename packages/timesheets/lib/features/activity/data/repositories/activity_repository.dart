71ce1e24d557674ef97473d095a8ec974e580046
shata <shatanikmahanty@gmail.com>
2023-02-28 23:52:51 +0530

    Cleanup imports

--- packages/timesheets/lib/features/activity/data/repositories/activity_repository.dart
+++ packages/timesheets/lib/features/activity/data/repositories/activity_repository.dart
@@ -1,6 +1,5 @@
 import 'package:timesheets/configurations/configurations.dart';
 import 'package:timesheets/features/app/app.dart';
-import 'package:timesheets/features/app/data/odoo/odoo_api_method.dart';
 
 ///Repository to fetch projects data
 class ActivityRepository extends OdooRpcRepositoryBase {
--- packages/timesheets/lib/features/tasks/data/repositories/tasks_repository.dart
+++ packages/timesheets/lib/features/tasks/data/repositories/tasks_repository.dart
@@ -1,6 +1,5 @@
 import 'package:timesheets/configurations/configurations.dart';
 import 'package:timesheets/features/app/app.dart';
-import 'package:timesheets/features/app/data/odoo/odoo_api_method.dart';
 import 'package:timesheets/features/tasks/tasks.dart';
 
 ///Repository to fetch task data
