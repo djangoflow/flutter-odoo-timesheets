043d6bd012391a989e580090cdd2957dd4e86d3f
adar2378 <saiful@apexive.com>
2023-07-05 22:13:53 +0600

    add different animation duration related constatns

--- packages/timesheets/lib/configurations/constants.dart
+++ packages/timesheets/lib/configurations/constants.dart
@@ -5,3 +5,8 @@ const appTitle = 'Odoo Timesheets';
 const appFontFamily = 'Inter';
 const designHeight = 852.0;
 const designWidth = 393.0;
+
+// animation constants
+const animationDurationDefault = Duration(milliseconds: 300);
+const animationDurationShort = Duration(milliseconds: 150);
+const animationDurationLong = Duration(milliseconds: 500);
