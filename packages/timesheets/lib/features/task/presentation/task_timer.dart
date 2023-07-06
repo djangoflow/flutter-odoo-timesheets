125c2e7e33f13919d1469077c13ce8d0504a83dd
adar2378 <saiful@apexive.com>
2023-07-07 00:16:57 +0600

    listen to status and pause timer when app goes to bg

--- packages/timesheets/lib/features/task/presentation/task_timer.dart
+++ packages/timesheets/lib/features/task/presentation/task_timer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timesheets/configurations/configurations.dart';
+import 'package:timesheets/features/app/app.dart';
 import 'package:timesheets/features/timer/blocs/timer_cubit/timer_cubit.dart';
 import 'package:timesheets/features/timer/presentation/timer_bloc_builder.dart';
 import 'package:timesheets/features/timer/presentation/timer_bloc_listener.dart';
@@ -65,11 +66,6 @@ class __TaskTimerState extends State<_TaskTimer> with TickerProviderStateMixin {
   }
 
   @override
-  void didUpdateWidget(covariant TaskTimer oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
   void dispose() {
     _timerCubit.close();
     controller.dispose();
@@ -77,14 +73,27 @@ class __TaskTimerState extends State<_TaskTimer> with TickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) => BlocProvider<TimerCubit>.value(
-        value: _timerCubit,
-        child: TimerBlocBuilder(
-          builder: (context, state) => widget.builder(
-            context,
-            state,
-            controller,
-            _timerCubit.tickDuration.inSeconds,
+  Widget build(BuildContext context) => AppLifeCycleListener(
+        onLifeCycleStateChanged: (appLifecycleState) {
+          if (appLifecycleState == AppLifecycleState.resumed) {
+            if (_timerCubit.state.status == TimerStatus.pausedByForce) {
+              _timerCubit.resumeTimer();
+            }
+          } else {
+            if (_timerCubit.state.status == TimerStatus.running) {
+              _timerCubit.forcePauseTimer();
+            }
+          }
+        },
+        child: BlocProvider<TimerCubit>.value(
+          value: _timerCubit,
+          child: TimerBlocBuilder(
+            builder: (context, state) => widget.builder(
+              context,
+              state,
+              controller,
+              _timerCubit.tickDuration.inSeconds,
+            ),
           ),
         ),
       );
@@ -181,88 +190,103 @@ class __TaskTimerSmallState extends State<_TaskTimerSmall> {
     final theme = Theme.of(context);
 
     return TimerBlocListener(
-      listenWhen: (prev, current) =>
-          prev.duration != current.duration || prev.status != current.status,
-      listener: (context, state) => widget.onTimerStateChange?.call(state),
-      child: TimerBlocBuilder(
-        builder: (context, state) {
-          final timerStatus = state.status;
+      listenWhen: (prev, current) => prev.status != current.status,
+      listener: (context, state) {
+        // Listen to status change and trigger animations
+        final timerStatus = state.status;
+        if (timerStatus == TimerStatus.running) {
+          widget.animationController.forward();
+        } else if ([
+          TimerStatus.initial,
+          TimerStatus.paused,
+          TimerStatus.pausedByForce
+        ].contains(timerStatus)) {
+          widget.animationController.reverse();
+        }
+      },
+      child: TimerBlocListener(
+        listenWhen: (prev, current) =>
+            prev.duration != current.duration || prev.status != current.status,
+        listener: (context, state) => widget.onTimerStateChange?.call(state),
+        child: TimerBlocBuilder(
+          builder: (context, state) {
+            final timerStatus = state.status;
 
-          return AnimatedContainer(
-            duration: animationDurationDefault,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kPadding.r * 8),
-              color: timerStatus == TimerStatus.running
-                  ? theme.colorScheme.primary
-                  : ElevationOverlay.applySurfaceTint(
-                      theme.colorScheme.primaryContainer,
-                      theme.colorScheme.surfaceTint,
-                      4,
-                    ),
-            ),
-            child: Padding(
-              padding: widget.padding ??
-                  EdgeInsets.fromLTRB(
-                    kPadding.w * 2,
-                    kPadding.h,
-                    kPadding.w,
-                    kPadding.h,
-                  ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: kPadding.w * 6,
-                    child: textStyleAnimation != null
-                        ? ValueListenableBuilder<TextStyle>(
-                            valueListenable: textStyleAnimation!,
-                            builder: (context, value, child) => Text(
-                              state.duration.timerString,
-                              style: value,
-                            ),
-                          )
-                        : const SizedBox(),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: colorAnimation != null
-                        ? ValueListenableBuilder<Color?>(
-                            valueListenable: colorAnimation!,
-                            builder: (context, value, child) => AnimatedIcon(
-                              icon: AnimatedIcons.play_pause,
-                              size: 32.w,
-                              progress: animation,
-                              // animated color based on controller's value, and it should be animated as well
-                              color: value,
-                            ),
-                          )
-                        : const SizedBox(),
-                    color: theme.colorScheme.onPrimary,
-                    onPressed: widget.disabled
-                        ? null
-                        : () {
-                            final timerCubit = context.read<TimerCubit>();
-                            if (widget.animationController.isCompleted &&
-                                timerStatus == TimerStatus.running) {
-                              timerCubit.pauseTimer();
-                              widget.animationController.reverse();
-                            } else if ([TimerStatus.initial, TimerStatus.paused]
-                                .contains(timerStatus)) {
-                              if (timerStatus == TimerStatus.initial) {
-                                timerCubit.startTimer();
-                              } else if (timerStatus == TimerStatus.paused) {
-                                timerCubit.resumeTimer();
-                              }
-                              widget.animationController.forward();
-                            }
-                          },
-                  ),
-                ],
+            return AnimatedContainer(
+              duration: animationDurationDefault,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(kPadding.r * 8),
+                color: timerStatus == TimerStatus.running
+                    ? theme.colorScheme.primary
+                    : ElevationOverlay.applySurfaceTint(
+                        theme.colorScheme.primaryContainer,
+                        theme.colorScheme.surfaceTint,
+                        4,
+                      ),
               ),
-            ),
-          );
-        },
+              child: Padding(
+                padding: widget.padding ??
+                    EdgeInsets.fromLTRB(
+                      kPadding.w * 2,
+                      kPadding.h,
+                      kPadding.w,
+                      kPadding.h,
+                    ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: kPadding.w * 6,
+                      child: textStyleAnimation != null
+                          ? ValueListenableBuilder<TextStyle>(
+                              valueListenable: textStyleAnimation!,
+                              builder: (context, value, child) => Text(
+                                state.duration.timerString,
+                                style: value,
+                              ),
+                            )
+                          : const SizedBox(),
+                    ),
+                    IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: colorAnimation != null
+                          ? ValueListenableBuilder<Color?>(
+                              valueListenable: colorAnimation!,
+                              builder: (context, value, child) => AnimatedIcon(
+                                icon: AnimatedIcons.play_pause,
+                                size: 32.w,
+                                progress: animation,
+                                // animated color based on controller's value, and it should be animated as well
+                                color: value,
+                              ),
+                            )
+                          : const SizedBox(),
+                      color: theme.colorScheme.onPrimary,
+                      onPressed: widget.disabled
+                          ? null
+                          : () {
+                              final timerCubit = context.read<TimerCubit>();
+                              if (timerStatus == TimerStatus.running) {
+                                timerCubit.pauseTimer();
+                              } else if ([
+                                TimerStatus.initial,
+                                TimerStatus.paused
+                              ].contains(timerStatus)) {
+                                if (timerStatus == TimerStatus.initial) {
+                                  timerCubit.startTimer();
+                                } else if (timerStatus == TimerStatus.paused) {
+                                  timerCubit.resumeTimer();
+                                }
+                              }
+                            },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
