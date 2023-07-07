adbd4a0dca174d5d765861a14b843249363564a0
adar2378 <saiful@apexive.com>
2023-07-08 01:00:34 +0600

    update DurationExtensions with proper formats

--- packages/timesheets/lib/utils/extensions/duration_extensions.dart
+++ packages/timesheets/lib/utils/extensions/duration_extensions.dart
@@ -1,8 +1,28 @@
 extension DurationExtension on Duration {
-  String get timerString {
+  String timerString(DurationFormat format) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    String twoDigitMinutes = twoDigits(inMinutes.remainder(60));
-    String twoDigitSeconds = twoDigits(inSeconds.remainder(60));
-    return '$twoDigitMinutes:$twoDigitSeconds';
+
+    final hours = inHours;
+    final minutes = format == DurationFormat.minutesSeconds
+        ? inMinutes
+        : inMinutes.remainder(60);
+    final seconds = inSeconds.remainder(60);
+
+    String twoDigitHours = twoDigits(hours);
+    String twoDigitMinutes = twoDigits(minutes);
+    String twoDigitSeconds = twoDigits(seconds);
+
+    if (format == DurationFormat.hoursMinutesSeconds) {
+      return '$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds';
+    } else if (format == DurationFormat.minutesSeconds) {
+      return '$twoDigitMinutes:$twoDigitSeconds';
+    } else {
+      throw ArgumentError('Invalid format provided.');
+    }
   }
+}
+
+enum DurationFormat {
+  hoursMinutesSeconds,
+  minutesSeconds,
 }
