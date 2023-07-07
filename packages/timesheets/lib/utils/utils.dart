0a7b7e6c4db7556666fd799b91c5b3e184e5a983
adar2378 <saiful@apexive.com>
2023-07-08 01:01:07 +0600

    update exports of utils

--- packages/timesheets/lib/utils/utils.dart
+++ packages/timesheets/lib/utils/utils.dart
@@ -2,3 +2,4 @@ export 'mixins/mounted_set_state_mixin.dart';
 export 'mixins/cubit_maybe_emit_mixin.dart';
 export 'extensions/duration_extensions.dart';
 export 'list_bloc_util.dart';
+export 'extensions/task_extensions.dart';
