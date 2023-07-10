b4468efd7520f6623cdcc4fac3c0a16180e5378b
adar2378 <saiful@apexive.com>
2023-07-10 11:39:18 +0600

    linter fix

--- packages/timesheets/lib/features/task/blocs/task_histories_list_cubit/task_histories_list_filter.dart
+++ packages/timesheets/lib/features/task/blocs/task_histories_list_cubit/task_histories_list_filter.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:list_bloc/list_bloc.dart';
 
 part 'task_histories_list_filter.freezed.dart';
 part 'task_histories_list_filter.g.dart';
