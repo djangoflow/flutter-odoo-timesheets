1e1d2a541a26922c9e0c0f00c82a7ea9baa2ded8
adar2378 <saiful@apexive.com>
2023-07-10 21:32:48 +0600

    add migration method

--- packages/timesheets/lib/features/app/data/db/app_database.dart
+++ packages/timesheets/lib/features/app/data/db/app_database.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:timesheets/features/task/task.dart';
@@ -34,6 +35,29 @@ class AppDatabase extends _$AppDatabase {
   // Migrations are covered later in the documentation.
   @override
   int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration => MigrationStrategy(
+        onUpgrade: (m, from, to) async {
+          // disable foreign_keys before migrations
+          await customStatement('PRAGMA foreign_keys = OFF');
+
+          // await transaction(() async {
+          //   // put your migration logic here
+          // });
+
+          // Assert that the schema is valid after migrations
+          if (kDebugMode) {
+            final wrongForeignKeys =
+                await customSelect('PRAGMA foreign_key_check').get();
+            assert(wrongForeignKeys.isEmpty,
+                '${wrongForeignKeys.map((e) => e.data)}');
+          }
+        },
+        beforeOpen: (details) async {
+          await customStatement('PRAGMA foreign_keys = ON');
+        },
+      );
 }
 
 // the LazyDatabase util lets us find the right location for the file async.
