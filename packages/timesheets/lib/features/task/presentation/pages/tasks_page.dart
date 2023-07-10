48ab072923c611a65b8d5edf95b75c87614db815
adar2378 <saiful@apexive.com>
2023-07-10 15:38:40 +0600

    use ValueNotifier instead of setState

--- packages/timesheets/lib/features/task/presentation/pages/tasks_page.dart
+++ packages/timesheets/lib/features/task/presentation/pages/tasks_page.dart
@@ -29,7 +29,7 @@ class TasksPage extends StatefulWidget with AutoRouteWrapper {
 
 class _TasksPageState extends State<TasksPage>
     with AutoRouteAwareStateMixin<TasksPage> {
-  bool? _isPageVisible;
+  ValueNotifier<bool>? _isPageVisible;
   final mainKey = const ValueKey('tasks_list_visiblity_detector');
 
   @override
@@ -42,126 +42,136 @@ class _TasksPageState extends State<TasksPage>
   @override
   void initState() {
     super.initState();
-    _isPageVisible = true;
+    _isPageVisible = ValueNotifier<bool>(true);
     VisibilityDetectorController.instance.updateInterval =
         animationDurationShort;
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Tasks'),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.settings_outlined,
-                size: kPadding.r * 4,
-              ),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(kPadding.r),
-          ),
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
-          child: Icon(
-            Icons.add,
-            size: kPadding.r * 4,
-          ),
-          onPressed: () async {
-            context.router.push(const TaskAddRoute());
-          },
-        ),
-        body: Padding(
-          padding: EdgeInsets.only(top: kPadding.h * 3),
-          child: RefreshIndicator(
-            onRefresh: () => context.read<TasksListCubit>().load(
-                  const TasksListFilter(),
+  void dispose() {
+    _isPageVisible?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => VisibilityDetector(
+        key: mainKey,
+        onVisibilityChanged: (info) {
+          if (info.visibleFraction == 0) {
+            _isPageVisible?.value = false;
+          } else {
+            _isPageVisible?.value = true;
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tasks'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.settings_outlined,
+                  size: kPadding.r * 4,
                 ),
-            child: BlocBuilder<TasksListCubit, TasksListState>(
-              builder: (context, state) => state.map(
-                (value) => VisibilityDetector(
-                  key: mainKey,
-                  onVisibilityChanged: (info) {
-                    if (info.visibleFraction == 0) {
-                      setState(() {
-                        _isPageVisible = false;
-                      });
-                    } else {
-                      setState(() {
-                        _isPageVisible = true;
-                      });
-                    }
-                  },
-                  child: !(_isPageVisible ?? false)
-                      ? const SizedBox(
-                          height: 20,
-                          child: Text('kaka'),
-                        )
-                      : ListView.separated(
-                          itemCount: value.data?.length ?? 0,
-                          separatorBuilder: (context, index) => SizedBox(
-                            height: kPadding.h,
-                          ),
-                          itemBuilder: (context, index) {
-                            final task = value.data![index];
-                            final elapsedTime = task.elapsedTime;
+              )
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(kPadding.r),
+            ),
+            backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+            foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+            child: Icon(
+              Icons.add,
+              size: kPadding.r * 4,
+            ),
+            onPressed: () async {
+              context.router.push(const TaskAddRoute());
+            },
+          ),
+          body: Padding(
+            padding: EdgeInsets.only(top: kPadding.h * 3),
+            child: RefreshIndicator(
+              onRefresh: () => context.read<TasksListCubit>().load(
+                    const TasksListFilter(),
+                  ),
+              child: BlocBuilder<TasksListCubit, TasksListState>(
+                builder: (context, state) => state.map(
+                  (value) => _isPageVisible == null
+                      ? const SizedBox()
+                      : ValueListenableBuilder<bool>(
+                          valueListenable: _isPageVisible!,
+                          builder: (context, isVisible, child) => !isVisible
+                              ? const SizedBox()
+                              : ListView.separated(
+                                  itemCount: value.data?.length ?? 0,
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(
+                                    height: kPadding.h,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    final task = value.data![index];
+                                    final elapsedTime = task.elapsedTime;
 
-                            return TaskListTile(
-                              key: ValueKey(task.id),
-                              title: Text(task.name),
-                              subtitle: Text(task.description ?? ''),
-                              elapsedTime: elapsedTime,
-                              initialTimerStatus:
-                                  TimerStatus.values[task.status],
-                              onTap: () {
-                                context.router
-                                    .push(TaskDetailsRoute(taskId: task.id));
-                              },
-                              onTimerResume: (context) {
-                                context.read<TimerCubit>().elapsedTime =
-                                    Duration(
-                                  seconds: elapsedTime,
-                                );
-                              },
-                              onTimerStateChange:
-                                  (timerState, tickDurationInSeconds) async {
-                                final isRunning =
-                                    timerState.status == TimerStatus.running;
-                                final updatableSeconds =
-                                    (isRunning ? tickDurationInSeconds : 0);
-                                final lastTickedValue = isRunning
-                                    ? DateTime.now()
-                                    : task.lastTicked;
+                                    return TaskListTile(
+                                      key: ValueKey(task.id),
+                                      title: Text(task.name),
+                                      subtitle: Text(task.description ?? ''),
+                                      elapsedTime: elapsedTime,
+                                      initialTimerStatus:
+                                          TimerStatus.values[task.status],
+                                      onTap: () {
+                                        context.router.push(
+                                            TaskDetailsRoute(taskId: task.id));
+                                      },
+                                      onTimerResume: (context) {
+                                        context.read<TimerCubit>().elapsedTime =
+                                            Duration(
+                                          seconds: elapsedTime,
+                                        );
+                                      },
+                                      onTimerStateChange: (timerState,
+                                          tickDurationInSeconds) async {
+                                        final isRunning = timerState.status ==
+                                            TimerStatus.running;
+                                        final updatableSeconds = (isRunning
+                                            ? tickDurationInSeconds
+                                            : 0);
+                                        final lastTickedValue = isRunning
+                                            ? DateTime.now()
+                                            : task.lastTicked;
 
-                                if (isRunning && task.firstTicked == null) {
-                                  task.copyWith(
-                                    firstTicked: Value(DateTime.now()),
-                                  );
-                                }
+                                        if (isRunning &&
+                                            task.firstTicked == null) {
+                                          task.copyWith(
+                                            firstTicked: Value(DateTime.now()),
+                                          );
+                                        }
 
-                                await context.read<TasksListCubit>().updateTask(
-                                      task.copyWith(
-                                        duration:
-                                            elapsedTime + updatableSeconds,
-                                        status: timerState.status.index,
-                                        lastTicked: Value(lastTickedValue),
-                                      ),
+                                        await context
+                                            .read<TasksListCubit>()
+                                            .updateTask(
+                                              task.copyWith(
+                                                duration: elapsedTime +
+                                                    updatableSeconds,
+                                                status: timerState.status.index,
+                                                lastTicked:
+                                                    Value(lastTickedValue),
+                                              ),
+                                            );
+                                      },
                                     );
-                              },
-                            );
-                          },
+                                  },
+                                ),
                         ),
-                ),
-                loading: (_) => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-                empty: (_) => const TasksPlaceHolder(),
-                error: (_) => const Center(
-                  child: Text('Error occurred!'),
+                  loading: (_) => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  empty: (_) => const TasksPlaceHolder(),
+                  error: (_) => const Center(
+                    child: Text('Error occurred!'),
+                  ),
                 ),
               ),
             ),
