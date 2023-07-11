41a79c23773d5c4dba4a555d1b3049900b069ec0
adar2378 <saiful@apexive.com>
2023-07-11 15:34:30 +0600

    Add authentication and odoo feature related repository and cubits

--- packages/timesheets/lib/timesheets_app_builder.dart
+++ packages/timesheets/lib/timesheets_app_builder.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:timesheets/features/authentication/authentication.dart';
+import 'package:timesheets/features/odoo/data/repositories/odoo_authentication_repository.dart';
+import 'package:timesheets/features/odoo/odoo.dart';
 import 'package:timesheets/features/task/task.dart';
 
 import 'configurations/router/route_observer.dart';
@@ -34,6 +37,14 @@ class TimesheetsAppBuilder extends AppBuilder {
                 context.read<AppDatabase>().timesheetsDao,
               ),
             ),
+            RepositoryProvider<OdooXmlRpcClient>(
+              create: (context) => OdooXmlRpcClient(),
+            ),
+            RepositoryProvider<OdooAuthenticationRepository>(
+              create: (context) => OdooAuthenticationRepository(
+                context.read<OdooXmlRpcClient>(),
+              ),
+            ),
           ],
           providers: [
             BlocProvider<AppCubit>(
@@ -46,6 +57,12 @@ class TimesheetsAppBuilder extends AppBuilder {
               ),
               lazy: false,
             ),
+            BlocProvider<AuthCubit>(
+              create: (context) => AuthCubit.instance
+                ..initialize(
+                  context.read<OdooAuthenticationRepository>(),
+                ),
+            ),
             BlocProvider<TasksListCubit>(
               create: (context) => TasksListCubit(
                 context.read<TasksRepository>(),
@@ -54,78 +71,99 @@ class TimesheetsAppBuilder extends AppBuilder {
                 ),
             )
           ],
-          builder: (context) => AppCubitConsumer(
-            listenWhen: (previous, current) =>
-                previous.environment != current.environment,
-            listener: (context, state) async {},
-            builder: (context, appState) => ScreenUtilInit(
-              designSize: const Size(designWidth, designHeight),
-              minTextAdapt: true,
-              builder: (context, child) => MaterialApp.router(
-                debugShowCheckedModeBanner: false,
-                scaffoldMessengerKey:
-                    DjangoflowAppSnackbar.scaffoldMessengerKey,
-                title: appTitle,
-                routeInformationParser: RouteParser(
-                  appRouter.matcher,
-                  includePrefixMatches: true,
-                ),
-                theme: AppTheme.light,
-                darkTheme: AppTheme.dark,
-                themeMode: appState.themeMode,
-                locale: Locale(appState.locale, ''),
-                supportedLocales: const [
-                  Locale('en', ''),
-                ],
-                localizationsDelegates: const [
-                  GlobalMaterialLocalizations.delegate,
-                  GlobalWidgetsLocalizations.delegate,
-                  GlobalCupertinoLocalizations.delegate,
-                ],
-                routerDelegate: appRouter.delegate(
-                  navigatorObservers: () => [
-                    AutoRouteObserver(),
-                    AppRouteObserver(),
-                  ],
-                  initialDeepLink: initialDeepLink, // only for Android and iOS
-                  // if initialDeepLink found then don't provide initialRoutes
-                  initialRoutes: kIsWeb || initialDeepLink != null
-                      ? null
-                      : [
-                          SplashRoute(backgroundColor: Colors.white),
-                        ],
-                ),
-                builder: (context, child) => MediaQuery(
-                  data: MediaQuery.of(context).copyWith(
-                    textScaleFactor: 1.0.sp,
+          builder: (context) => LoginListenerWrapper(
+            initialUser: context.read<AuthCubit>().state.odooUser,
+            onLogin: (context, user) {
+              final authCubit = context.read<AuthCubit>();
+              final authState = authCubit.state;
+              final odooCredentials = authState.odooCredentials;
+
+              if (odooCredentials?.password == null) {
+                authCubit.logout();
+              } else {
+                context.read<OdooXmlRpcClient>().updateCredentials(
+                      password: odooCredentials?.password,
+                      id: user.id,
+                      baseUrl: odooCredentials?.serverUrl,
+                      db: odooCredentials?.db,
+                    );
+              }
+            },
+            child: AppCubitConsumer(
+              listenWhen: (previous, current) =>
+                  previous.environment != current.environment,
+              listener: (context, state) async {},
+              builder: (context, appState) => ScreenUtilInit(
+                designSize: const Size(designWidth, designHeight),
+                minTextAdapt: true,
+                builder: (context, child) => MaterialApp.router(
+                  debugShowCheckedModeBanner: false,
+                  scaffoldMessengerKey:
+                      DjangoflowAppSnackbar.scaffoldMessengerKey,
+                  title: appTitle,
+                  routeInformationParser: RouteParser(
+                    appRouter.matcher,
+                    includePrefixMatches: true,
                   ),
-                  child: AppResponsiveLayoutBuilder(
-                    background: Container(
-                      color: Colors.black87, // use theme color
+                  theme: AppTheme.light,
+                  darkTheme: AppTheme.dark,
+                  themeMode: appState.themeMode,
+                  locale: Locale(appState.locale, ''),
+                  supportedLocales: const [
+                    Locale('en', ''),
+                  ],
+                  localizationsDelegates: const [
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                  ],
+                  routerDelegate: appRouter.delegate(
+                    navigatorObservers: () => [
+                      AutoRouteObserver(),
+                      AppRouteObserver(),
+                    ],
+                    initialDeepLink:
+                        initialDeepLink, // only for Android and iOS
+                    // if initialDeepLink found then don't provide initialRoutes
+                    initialRoutes: kIsWeb || initialDeepLink != null
+                        ? null
+                        : [
+                            SplashRoute(backgroundColor: Colors.white),
+                          ],
+                  ),
+                  builder: (context, child) => MediaQuery(
+                    data: MediaQuery.of(context).copyWith(
+                      textScaleFactor: 1.0.sp,
                     ),
-                    child: SandboxBanner(
-                      isSandbox: appState.environment == AppEnvironment.sandbox,
-                      child: child != null
-                          ? kIsWeb
-                              ? child
-                              : AppLinksCubitListener(
-                                  listenWhen: (previous, current) =>
-                                      current != null,
-                                  listener: (context, appLink) {
-                                    final path = appLink?.path;
-                                    if (path != null) {
-                                      appRouter.navigateNamed(
-                                        path,
-                                        onFailure: (failure) {
-                                          appRouter
-                                              .navigate(const TasksRouter());
-                                        },
-                                      );
-                                    }
-                                  },
-                                  child: child,
-                                )
-                          : const Offstage(),
+                    child: AppResponsiveLayoutBuilder(
+                      background: Container(
+                        color: Colors.black87, // use theme color
+                      ),
+                      child: SandboxBanner(
+                        isSandbox:
+                            appState.environment == AppEnvironment.sandbox,
+                        child: child != null
+                            ? kIsWeb
+                                ? child
+                                : AppLinksCubitListener(
+                                    listenWhen: (previous, current) =>
+                                        current != null,
+                                    listener: (context, appLink) {
+                                      final path = appLink?.path;
+                                      if (path != null) {
+                                        appRouter.navigateNamed(
+                                          path,
+                                          onFailure: (failure) {
+                                            appRouter
+                                                .navigate(const TasksRouter());
+                                          },
+                                        );
+                                      }
+                                    },
+                                    child: child,
+                                  )
+                            : const Offstage(),
+                      ),
                     ),
                   ),
                 ),
