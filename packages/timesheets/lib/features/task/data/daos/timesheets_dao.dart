670e6efa895043975556c7fb8c103fdb98cef306
adar2378 <saiful@apexive.com>
2023-07-12 22:34:31 +0600

    adjust ordering

--- packages/timesheets/lib/features/task/data/daos/timesheets_dao.dart
+++ packages/timesheets/lib/features/task/data/daos/timesheets_dao.dart
@@ -18,10 +18,19 @@ class TimesheetsDao extends DatabaseAccessor<AppDatabase>
 
   Future<void> deleteTimesheetsByTaskId(int taskId) =>
       (delete(timesheets)..where((th) => th.taskId.equals(taskId))).go();
-
+  // in latest item at top order
   Future<List<Timesheet>> getTimesheets(int? taskId) => taskId == null
-      ? select(timesheets).get()
-      : (select(timesheets)..where((th) => th.taskId.equals(taskId))).get();
+      ? (select(timesheets)
+            ..orderBy([
+              (th) => OrderingTerm(expression: th.id, mode: OrderingMode.desc),
+            ]))
+          .get()
+      : (select(timesheets)
+            ..where((th) => th.taskId.equals(taskId))
+            ..orderBy([
+              (th) => OrderingTerm(expression: th.id, mode: OrderingMode.desc),
+            ]))
+          .get();
 
   Future<void> updateTimesheet(Timesheet timesheet) =>
       update(timesheets).replace(timesheet);
