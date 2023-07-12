0b758233142b6865d61da49597104ecbe48e37b1
adar2378 <saiful@apexive.com>
2023-07-13 05:02:58 +0600

    wip-add sync all items on app start method

--- packages/timesheets/lib/features/task/blocs/tasks_list_cubit/tasks_list_cubit.dart
+++ packages/timesheets/lib/features/task/blocs/tasks_list_cubit/tasks_list_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:list_bloc/list_bloc.dart';
 import 'package:timesheets/features/app/app.dart';
+import 'package:timesheets/features/odoo/data/repositories/odoo_timesheet_repository.dart';
+
 import 'package:timesheets/features/task/data/models/task_with_project.dart';
 import 'package:timesheets/features/task/data/repositories/tasks_repository.dart';
 import 'package:timesheets/utils/utils.dart';
@@ -11,8 +13,11 @@ typedef TasksListState = Data<List<TaskWithProject>, TasksListFilter>;
 class TasksListCubit extends ListCubit<TaskWithProject, TasksListFilter>
     with CubitMaybeEmit {
   final TasksRepository tasksRepository;
-  TasksListCubit(this.tasksRepository)
-      : super(
+  final OdooTimesheetRepository odooTimesheetRepository;
+  TasksListCubit({
+    required this.tasksRepository,
+    required this.odooTimesheetRepository,
+  }) : super(
           ListBlocUtil.listLoader<TaskWithProject, TasksListFilter>(
             loader: ([filter]) => tasksRepository.getTasksWithProjects(
                 filter?.limit ?? TasksListFilter.kPageSize, filter?.offset),
@@ -40,6 +45,15 @@ class TasksListCubit extends ListCubit<TaskWithProject, TasksListFilter>
           ],
         ),
       );
+    }
+  }
+
+  // TODO 1: Add a method to sync all tasks with onlineId from Odoo
+  Future<void> syncAllTasksWithOnlineIdFromOdoo() async {
+    final tasks = await tasksRepository.getAllTasks();
+    for (final task in tasks) {
+      final onlineId = task.onlineId;
+      if (onlineId != null) {}
     }
   }
 
