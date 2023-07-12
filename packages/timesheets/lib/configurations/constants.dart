787d230c75ceb8acd884a5eee5100d3e9a65bace
adar2378 <saiful@apexive.com>
2023-07-13 05:10:10 +0600

    Odoo timesheets -> Apexive timesheets

--- packages/timesheets/lib/configurations/constants.dart
+++ packages/timesheets/lib/configurations/constants.dart
@@ -1,7 +1,7 @@
 // Global APP - specific constants
 const sentryDSN = '';
 const appLogoSvgPath = 'assets/app_logo/logo.svg';
-const appTitle = 'Odoo Timesheets';
+const appTitle = 'Apexive Timesheets';
 const appFontFamily = 'Inter';
 const designHeight = 852.0;
 const designWidth = 393.0;
