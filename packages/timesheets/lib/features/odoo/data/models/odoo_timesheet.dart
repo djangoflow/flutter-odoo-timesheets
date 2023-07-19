e42cf44c0e9a2b8de49a122184b1ef53de3725b7
adar2378 <saiful@apexive.com>
2023-07-20 00:43:14 +0600

    update OdooTimesheet model

--- packages/timesheets/lib/features/odoo/data/models/odoo_timesheet.dart
+++ packages/timesheets/lib/features/odoo/data/models/odoo_timesheet.dart
@@ -2,6 +2,8 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../odoo_null_field_converter.dart';
+
 part 'odoo_timesheet.freezed.dart';
 part 'odoo_timesheet.g.dart';
 
@@ -12,8 +14,18 @@ class OdooTimesheet with _$OdooTimesheet {
     required int id,
     @JsonKey(name: 'project_id') required List<Object> project,
     @JsonKey(name: 'task_id') required List<Object> task,
-    @JsonKey(name: 'date_time') required DateTime startTime,
-    @JsonKey(name: 'date_time_end') required DateTime endTime,
+    @JsonKey(
+      name: 'date_time',
+      fromJson: OdooNullValueJsonConverter.fromJsonOrNull<DateTime>,
+      toJson: OdooNullValueJsonConverter.toJsonOrNull<DateTime>,
+    )
+    DateTime? startTime,
+    @JsonKey(
+      name: 'date_time_end',
+      fromJson: OdooNullValueJsonConverter.fromJsonOrNull<DateTime>,
+      toJson: OdooNullValueJsonConverter.toJsonOrNull<DateTime>,
+    )
+    DateTime? endTime,
     @JsonKey(name: 'unit_amount') required double unitAmount,
   }) = _OdooTimesheet;
 
