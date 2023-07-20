1cb5e5dedf14726248d8b11033da409b0f6c7035
adar2378 <saiful@apexive.com>
2023-07-21 02:37:52 +0600

    fix icons in appbar

--- packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
+++ packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
@@ -1,6 +1,7 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:timesheets/configurations/configurations.dart';
 
 @RoutePage(name: 'HomeTabRouter')
@@ -8,25 +9,30 @@ class HomeTabRouterPage extends StatelessWidget {
   const HomeTabRouterPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      appBarBuilder: (context, tabsRouter) => AppBar(
-        title: Text(
-          tabsRouter.current.path.capitalize,
+  Widget build(BuildContext context) => AutoTabsScaffold(
+        appBarBuilder: (context, tabsRouter) => AppBar(
+          title: Text(
+            tabsRouter.current.path.capitalize,
+          ),
+          actions: [
+            if (tabsRouter.activeIndex == 0)
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.add),
+                iconSize: kPadding.h * 4,
+              )
+          ],
+          centerTitle: false,
         ),
-        centerTitle: true,
-        leading: const SizedBox(),
-      ),
-      routes: [
-        TasksRoute(),
-        ProjectsRoute(),
-        SettingsRoute(),
-      ],
-      bottomNavigationBuilder: (context, tabsRouter) {
-        return BottomNavigationBar(
+        routes: const [
+          TasksRoute(),
+          ProjectsRoute(),
+          SettingsRoute(),
+        ],
+        bottomNavigationBuilder: (context, tabsRouter) => BottomNavigationBar(
           currentIndex: tabsRouter.activeIndex,
           onTap: tabsRouter.setActiveIndex,
-          items: [
+          items: const [
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.clock),
               activeIcon: Icon(CupertinoIcons.clock_fill),
@@ -43,8 +49,6 @@ class HomeTabRouterPage extends StatelessWidget {
               label: 'Settings',
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+      );
 }
