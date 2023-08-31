210d5c1546d4f863d7e7a1efc58480f8c37421d3
adar2378 <saiful@apexive.com>
2023-08-31 12:17:28 +0600

    refactor: update bg color and use scaffold

--- packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
+++ packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
@@ -7,45 +7,58 @@ class HomeTabRouterPage extends StatelessWidget {
   const HomeTabRouterPage({super.key});
 
   @override
-  Widget build(BuildContext context) => AutoTabsRouter(
-        builder: (context, child) {
-          final tabsRouter = context.tabsRouter;
-          return IconButtonTheme(
-            data: AppTheme.getFilledIconButtonTheme(Theme.of(context)),
-            child: Column(
-              children: [
-                Expanded(
-                  child: child,
-                ),
-                BottomNavigationBar(
-                  currentIndex: tabsRouter.activeIndex,
-                  onTap: tabsRouter.setActiveIndex,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.clock),
-                      activeIcon: Icon(CupertinoIcons.clock_fill),
-                      label: 'Timers',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.briefcase),
-                      activeIcon: Icon(CupertinoIcons.briefcase_fill),
-                      label: 'Projects',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(CupertinoIcons.settings),
-                      activeIcon: Icon(CupertinoIcons.settings_solid),
-                      label: 'Settings',
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
-        routes: const [
-          TimesheetsRoute(),
-          ProjectsTabRouter(),
-          SettingsRoute(),
-        ],
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).colorScheme.surfaceVariant,
+              Theme.of(context).colorScheme.surface,
+            ],
+            stops: const [.15, .85],
+          ),
+        ),
+        child: AutoTabsRouter(
+          builder: (context, child) {
+            final tabsRouter = context.tabsRouter;
+            return IconButtonTheme(
+              data: AppTheme.getFilledIconButtonTheme(Theme.of(context)),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: child,
+                  ),
+                  BottomNavigationBar(
+                    currentIndex: tabsRouter.activeIndex,
+                    onTap: tabsRouter.setActiveIndex,
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Icon(CupertinoIcons.clock),
+                        activeIcon: Icon(CupertinoIcons.clock_fill),
+                        label: 'Timers',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(CupertinoIcons.briefcase),
+                        activeIcon: Icon(CupertinoIcons.briefcase_fill),
+                        label: 'Projects',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(CupertinoIcons.settings),
+                        activeIcon: Icon(CupertinoIcons.settings_solid),
+                        label: 'Settings',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+          routes: const [
+            TimesheetsRoute(),
+            ProjectsTabRouter(),
+            SettingsRoute(),
+          ],
+        ),
       );
 }
