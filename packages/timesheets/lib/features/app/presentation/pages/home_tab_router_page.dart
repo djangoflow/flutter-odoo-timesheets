d27525bf2942f20913bc7d25b607194a67153f84
adar2378 <saiful@apexive.com>
2023-09-04 00:56:17 +0600

    refactor: remove not needed icon theme

--- packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
+++ packages/timesheets/lib/features/app/presentation/pages/home_tab_router_page.dart
@@ -22,36 +22,33 @@ class HomeTabRouterPage extends StatelessWidget {
         child: AutoTabsRouter(
           builder: (context, child) {
             final tabsRouter = context.tabsRouter;
-            return IconButtonTheme(
-              data: AppTheme.getFilledIconButtonTheme(Theme.of(context)),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: child,
-                  ),
-                  BottomNavigationBar(
-                    currentIndex: tabsRouter.activeIndex,
-                    onTap: tabsRouter.setActiveIndex,
-                    items: const [
-                      BottomNavigationBarItem(
-                        icon: Icon(CupertinoIcons.clock),
-                        activeIcon: Icon(CupertinoIcons.clock_fill),
-                        label: 'Timers',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(CupertinoIcons.briefcase),
-                        activeIcon: Icon(CupertinoIcons.briefcase_fill),
-                        label: 'Projects',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(CupertinoIcons.settings),
-                        activeIcon: Icon(CupertinoIcons.settings_solid),
-                        label: 'Settings',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            return Column(
+              children: [
+                Expanded(
+                  child: child,
+                ),
+                BottomNavigationBar(
+                  currentIndex: tabsRouter.activeIndex,
+                  onTap: tabsRouter.setActiveIndex,
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(CupertinoIcons.clock),
+                      activeIcon: Icon(CupertinoIcons.clock_fill),
+                      label: 'Timers',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(CupertinoIcons.briefcase),
+                      activeIcon: Icon(CupertinoIcons.briefcase_fill),
+                      label: 'Projects',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(CupertinoIcons.settings),
+                      activeIcon: Icon(CupertinoIcons.settings_solid),
+                      label: 'Settings',
+                    ),
+                  ],
+                ),
+              ],
             );
           },
           routes: const [
