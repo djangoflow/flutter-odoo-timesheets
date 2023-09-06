f3f53b58928702136240d80d9c53760a4823f161
adar2378 <saiful@apexive.com>
2023-09-06 22:19:35 +0600

    feat: add simple in-memory backend

--- packages/timesheets/lib/features/in_memory_backend/data/repositories/in_memory_project_repository.dart
+++ packages/timesheets/lib/features/in_memory_backend/data/repositories/in_memory_project_repository.dart
@@ -1,9 +1,13 @@
 import 'package:collection/collection.dart';
+import 'package:timesheets/features/in_memory_backend/in_memory_backend.dart';
 import 'package:timesheets/features/project/project.dart';
 import 'package:uuid/uuid.dart';
 
 class InMemoryProjectRepository implements ProjectRepository {
-  final List<Project> _projects = [];
+  final InMemoryBackend _backend;
+
+  InMemoryProjectRepository({required InMemoryBackend backend})
+      : _backend = backend;
 
   @override
   Future<int> createItem(Project item) {
@@ -11,30 +15,30 @@ class InMemoryProjectRepository implements ProjectRepository {
 
     // assign id
     final updatedItem = item.copyWith(id: uuid.v4().hashCode);
-    _projects.add(updatedItem);
+    _backend.projects.add(updatedItem);
     // return id
     return Future.value(updatedItem.id);
   }
 
   @override
   Future<int> deleteItem(int id) {
-    _projects.removeWhere((element) => element.id == id);
+    _backend.projects.removeWhere((element) => element.id == id);
     return Future.value(id);
   }
 
   @override
-  Future<List<Project>> getAllItems() => Future.value(_projects);
+  Future<List<Project>> getAllItems() => Future.value(_backend.projects);
 
   @override
   Future<Project?> getItemById(int id) => Future.value(
-        _projects.firstWhereOrNull((element) => element.id == id),
+        _backend.projects.firstWhereOrNull((element) => element.id == id),
       );
 
   @override
   Future<List<Project>> getPaginatedItems(
       [ProjectPaginatedFilter? filter]) async {
-    final projects =
-        _projects.skip(filter?.offset ?? ProjectPaginatedFilter.kPageSize);
+    final projects = _backend.projects
+        .skip(filter?.offset ?? ProjectPaginatedFilter.kPageSize);
     if (filter?.limit != null) {
       return projects.take(filter!.limit).toList();
     }
@@ -43,10 +47,11 @@ class InMemoryProjectRepository implements ProjectRepository {
 
   @override
   Future<void> updateItem(Project item) async {
-    final index = _projects.indexWhere((element) => element.id == item.id);
+    final index =
+        _backend.projects.indexWhere((element) => element.id == item.id);
     if (index == -1) {
       throw Exception('Item not found');
     }
-    _projects[index] = item;
+    _backend.projects[index] = item;
   }
 }
--- packages/timesheets/lib/features/in_memory_backend/in_memory_backend.dart
+++ packages/timesheets/lib/features/in_memory_backend/in_memory_backend.dart
@@ -1,1 +1,10 @@
+import 'package:timesheets/features/project/project.dart';
+import 'package:timesheets/features/task/task.dart';
+
 export 'data/repositories/in_memory_project_repository.dart';
+
+/// InMemoryBackend is a simple in-memory backend for the timesheets app.
+class InMemoryBackend {
+  final List<Project> projects = [];
+  final List<Task> tasks = [];
+}
