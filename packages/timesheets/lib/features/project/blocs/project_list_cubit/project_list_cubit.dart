6e9f317567f263d0208a8706699c98619080034c
adar2378 <saiful@apexive.com>
2023-09-07 00:26:28 +0600

    feat: add Favorite and Local ProjectListCubit

--- packages/timesheets/lib/features/project/blocs/project_list_cubit/project_list_cubit.dart
+++ packages/timesheets/lib/features/project/blocs/project_list_cubit/project_list_cubit.dart
@@ -1,7 +1,19 @@
+import 'package:list_bloc/list_bloc.dart';
 import 'package:timesheets/features/app/app.dart';
 import 'package:timesheets/features/project/project.dart';
 
-class ProjectListCubit extends CrudListCubit<Project, ProjectPaginatedFilter> {
+typedef ProjectListCubitType = CrudListCubit<Project, ProjectPaginatedFilter>;
+typedef ProjectListCubitState = Data<List<Project>, ProjectPaginatedFilter>;
+
+class ProjectListCubit extends ProjectListCubitType {
   // Plug in InMemoryRepository or RemoteRepository etc here
   ProjectListCubit({required super.repository});
+}
+
+class FavoriteProjectListCubit extends ProjectListCubit {
+  FavoriteProjectListCubit({required super.repository});
+}
+
+class LocalProjectListCubit extends ProjectListCubit {
+  LocalProjectListCubit({required super.repository});
 }
