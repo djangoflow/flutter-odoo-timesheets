ab9a3260a6638f9d51dfabf5f5fcdab568537e45
adar2378 <saiful@apexive.com>
2023-09-06 15:06:35 +0600

    chore: linter

--- packages/timesheets/lib/features_refactored/task/data/daos/tasks_dao.dart
+++ packages/timesheets/lib/features_refactored/task/data/daos/tasks_dao.dart
@@ -1,6 +1,5 @@
 import 'package:drift/drift.dart';
 import 'package:timesheets/features/app/data/db/app_database.dart';
-import 'package:timesheets/features/project/data/database_tables/project.dart';
 import 'package:timesheets/features/task/data/database_tables/task.dart';
 import 'package:timesheets/features_refactored/app/data/dao.dart';
 
