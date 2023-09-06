7ca9be0ec54e78b137fed5738fcdfcb4334a1f00
adar2378 <saiful@apexive.com>
2023-09-07 00:26:45 +0600

    refactor: update exports for project feat

--- packages/timesheets/lib/features/project/project.dart
+++ packages/timesheets/lib/features/project/project.dart
@@ -2,3 +2,6 @@ export 'blocs/project_list_cubit/project_list_cubit.dart';
 export 'blocs/project_list_cubit/project_paginated_filter.dart';
 export 'data/models/project.dart';
 export 'data/repositories/project_repository.dart';
+export 'presentation/project_list_view.dart';
+export 'presentation/placeholders/favorite_projects_placeholder.dart';
+export 'presentation/placeholders/local_projects_placeholder.dart';
