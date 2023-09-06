9f75b492f13a60e81ad3dbb0025c3563502fc71e
adar2378 <saiful@apexive.com>
2023-09-07 00:27:05 +0600

    feat: add color extension

---
+++ packages/timesheets/lib/utils/extensions/project_color_extensions.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:timesheets/configurations/configurations.dart';
+
+extension ProjectColorExtensions on int? {
+  Color get toColorFromColorIndex {
+    switch (this) {
+      case 0:
+        return OdooColors.white.color;
+      case 1:
+        return OdooColors.green.color;
+      case 2:
+        return OdooColors.red.color;
+      case 3:
+        return OdooColors.yellow.color;
+      case 4:
+        return OdooColors.orange.color;
+      case 5:
+        return OdooColors.lightBlue.color;
+      case 6:
+        return OdooColors.mediumBlue.color;
+      case 7:
+        return OdooColors.darkBlue.color;
+      case 8:
+        return OdooColors.purple.color;
+      case 11:
+        return OdooColors.darkPurple.color;
+      case 12:
+        return OdooColors.fushia.color;
+      case 13:
+        return OdooColors.salmonPink.color;
+      default:
+        return OdooColors.white.color;
+    }
+  }
+
+  OdooColors get toOdooColorFromColorIndex {
+    switch (this) {
+      case 0:
+        return OdooColors.white;
+      case 1:
+        return OdooColors.green;
+      case 2:
+        return OdooColors.red;
+      case 3:
+        return OdooColors.yellow;
+      case 4:
+        return OdooColors.orange;
+      case 5:
+        return OdooColors.lightBlue;
+      case 6:
+        return OdooColors.mediumBlue;
+      case 7:
+        return OdooColors.darkBlue;
+      case 8:
+        return OdooColors.purple;
+      case 11:
+        return OdooColors.darkPurple;
+      case 12:
+        return OdooColors.fushia;
+      case 13:
+        return OdooColors.salmonPink;
+      default:
+        return OdooColors.white;
+    }
+  }
+}
--- packages/timesheets/lib/utils/utils.dart
+++ packages/timesheets/lib/utils/utils.dart
@@ -5,3 +5,4 @@ export 'list_bloc_util.dart';
 export 'extensions/int_extensions.dart';
 export 'extensions/date_time_extensions.dart';
 export 'mixins/cubit_active_state_mixin.dart';
+export 'extensions/project_color_extensions.dart';
