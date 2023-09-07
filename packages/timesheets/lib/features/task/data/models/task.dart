be2499700162a41b3d12beaa7919fe1bf6b5268d
adar2378 <saiful@apexive.com>
2023-09-07 18:07:42 +0600

    refactor: update JsonKey for variables

--- packages/timesheets/lib/features/task/data/models/task.dart
+++ packages/timesheets/lib/features/task/data/models/task.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'task.freezed.dart';
@@ -7,17 +9,17 @@ part 'task.g.dart';
 class Task with _$Task {
   const factory Task({
     int? id,
-    int? projectId,
-    int? stageId,
+    @JsonKey(name: 'project_id') required int projectId,
+    @JsonKey(name: 'stage_id') int? stageId,
     String? name,
     String? description,
     String? priority,
-    DateTime? dateStart,
-    DateTime? dateEnd,
-    DateTime? dateDeadline,
+    @JsonKey(name: 'date_start') DateTime? dateStart,
+    @JsonKey(name: 'date_end') DateTime? dateEnd,
+    @JsonKey(name: 'date_deadline') DateTime? dateDeadline,
     bool? active,
-    DateTime? createdAt,
-    DateTime? updatedAt,
+    @JsonKey(name: 'created_at') DateTime? createdAt,
+    @JsonKey(name: 'updated_at') DateTime? updatedAt,
   }) = _Task;
 
   factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
