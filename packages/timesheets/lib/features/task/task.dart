f490643eb8ad1a58d50a15dfb5df5f666dc323ef
adar2378 <saiful@apexive.com>
2023-09-07 18:12:17 +0600

    feat: update exports

--- packages/timesheets/lib/features/task/task.dart
+++ packages/timesheets/lib/features/task/task.dart
@@ -1,3 +1,4 @@
 export 'data/models/task.dart';
 export 'blocs/project_list_cubit/task_list_cubit.dart';
 export 'data/repositories/task_repository.dart';
+export 'data/repositories/task_with_project_repository.dart';
--- packages/timesheets/lib/features/timesheet/timesheet.dart
+++ packages/timesheets/lib/features/timesheet/timesheet.dart
@@ -3,3 +3,5 @@ export 'blocs/timesheet_list_cubit/timesheet_paginated_filter.dart';
 export 'data/repositories/timesheet_repository.dart';
 export 'presentation/placeholders/favorite_timesheets_placeholder.dart';
 export 'presentation/placeholders/local_timesheets_placeholder.dart';
+export 'data/models/timesheet_with_task_project.dart';
+export 'data/repositories/timesheet_with_task_project_repository.dart';
